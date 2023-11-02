dc9c7c75d47ebc64ebb0930a11fbc5b78f0b41a3
Addie Rudy <awsrudy@amazon.com>
2023-11-02 15:24:42 -0400

    Handling Condition Error in RSS parser

--- lib/rag-engines/data-import/functions/rss-ingestor/index.py
+++ lib/rag-engines/data-import/functions/rss-ingestor/index.py
@@ -1,6 +1,7 @@
 import os
 import boto3
 import requests
+from botocore.exceptions import ClientError
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
 import feedparser
@@ -20,31 +21,39 @@ def add_feed_entries_to_table(feed_contents, rss_feed_url, rag_workspace_id):
     logger.info(f"Adding Parsed RSS Entries to DynamoDB Table")
     for feed_item in feed_contents['entries']:
         logger.info(f"Adding Post {feed_item['link']}")
-        dynamo_response = dynamodb.put_item(
-            TableName=rss_feed_items_table,
-            Item={
-                'PostURL': {
-                    'S': feed_item['link']
+        try:
+            dynamo_response = dynamodb.put_item(
+                TableName=rss_feed_items_table,
+                Item={
+                    'PostURL': {
+                        'S': feed_item['link']
+                    },
+                    'FeedURL': {
+                        'S': rss_feed_url
+                    },
+                    'RAGWorkspaceId': {
+                        'S': rag_workspace_id
+                    },
+                    'PublishDate': {
+                        'S': feed_item['published']
+                    },
+                    'Title': {
+                        'S': feed_item['title']
+                    },
+                    'Status': {
+                        'S': 'PENDING'
+                    }
                 },
-                'FeedURL': {
-                    'S': rss_feed_url
-                },
-                'RAGWorkspaceId': {
-                    'S': rag_workspace_id
-                },
-                'PublishDate': {
-                    'S': feed_item['published']
-                },
-                'Title': {
-                    'S': feed_item['title']
-                },
-                'Status': {
-                    'S': 'PENDING'
-                }
-            },
-            ConditionExpression='attribute_not_exists(PostURL)'
-        )
-        logger.info(f"DynamoDB Response: {dynamo_response}")
+                ConditionExpression='attribute_not_exists(PostURL)'
+            )
+            logger.info(f"DynamoDB Response: {dynamo_response}")
+        except ClientError as e:
+            if e.response['Error']['Code']!='ConditionalCheckFailedException':
+                logger.error(f"Error adding post to table: {e}")
+            else:
+                logger.info(f"Post already exists in table")
+            
+        
     logger.info(f"Finished Adding Parsed RSS Entries to DynamoDB Table")
 
 @logger.inject_lambda_context(log_event=True)
